e97ea1bcd4c47608abab7ce61e402ed2b842100b
Cartrel <72543424+CartelSharp@users.noreply.github.com>
2024-02-19 00:04:59 +0200

    initial commit

---
+++ Javascript/routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('n00b');
+});
+
+router.get('/toka', function(req, res) 
+  {
+    res.send('Olen toka reitti');
+  }
+)
+
+router.get('/kolmas/:name', function(req, res) 
+  {
+    res.send('Heippa ' + req.params.name);
+    console.log(req.params.name)
+  }
+)
+
+router.get('/neljas/:fname/:lname', function(req, res) 
+  {
+    res.send('Heippa ' + req.params.fname + ' ' + req.params.lname);
+    console.log(req.params.fname+' '+req.params.lname);
+
+  }
+)
+
+router.post('/', function(req, res)
+  {
+    res.send(req.body.fname+' '+req.body.lname);
+    console.log(req.body.fname+' '+req.body.lname);
+
+  }
+);
+
+module.exports = router;
